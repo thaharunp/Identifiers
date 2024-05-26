2bca8ce260e414cbc8e9457c932ef229546ee72e
sadhakunps <sadaqunps@gmail.com>
2024-05-26 17:39:35 +0530

    programs

---
+++ Account.java
@@ -0,0 +1,3 @@
+public class Account{
+
+} // main method not found in class Account, please define the main method 
---
+++ Test-2.java
@@ -0,0 +1,8 @@
+class Test{
+    int eid =101;
+
+    String ename = "thaharun nasir";
+    public void getDetails(){}
+    public void true1(){}
+    String string = "thaiba";
+}
